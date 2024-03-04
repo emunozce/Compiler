18266b88ca2765f0f1662920fc881b1325ad0c4c
Emmanuel Munoz <emunozce80@gmail.com>
2024-03-04 03:19:38 -0600

    Add search and folder icons, remove compiler_v2.py

--- src/main.py
+++ src/main.py
@@ -9,9 +9,12 @@ from PyQt5.QtWidgets import (
     QTreeView,
     QMenu,
     QVBoxLayout,
+    QTabWidget,
+    QLabel,
+    QFileDialog,
 )
-from PyQt5.QtCore import Qt, QDir
-from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt, QDir, QSize, QModelIndex
+from PyQt5.QtGui import QFont, QPixmap
 
 from PyQt5.Qsci import QsciScintilla
 
@@ -24,6 +27,8 @@ class MainWindow(QMainWindow):
     def __init__(self):  # Constructor
         super(QMainWindow, self).__init__()  # Call the constructor of the parent class
         self.init_ui()  # Call the method to initialize the UI
+
+        self.current_file = None
 
     def init_ui(self):  # Method to initialize the UI
         self.setWindowTitle("IDE")  # Set the title of the window
@@ -44,7 +49,69 @@ class MainWindow(QMainWindow):
             return file.read()
 
     def get_editor(self) -> QsciScintilla:
-        pass
+        # instance
+        editor = QsciScintilla()
+
+        # Encoding
+        editor.setUtf8(True)
+
+        # Set the font of the editor
+        editor.setFont(self.window_font)
+
+        # Brace matching
+        editor.setBraceMatching(QsciScintilla.SloppyBraceMatch)
+
+        # identation
+        editor.setIndentationGuides(True)
+        editor.setTabWidth(4)
+        editor.setIndentationsUseTabs(False)
+        editor.setAutoIndent(True)
+
+        # EOL
+        editor.setEolMode(QsciScintilla.EolUnix)
+        editor.setEolVisibility(False)
+
+        return editor
+
+    def is_binary(
+        self, path: Path
+    ) -> bool:  # Check if a file is binary (e.g. image, video, etc.)
+        with open(path, "rb") as file:
+            return b"\0" in file.read(1024)  # Check for null bytes
+
+    def set_new_tab(self, path: Path, is_new_file=False):
+        editor = self.get_editor()
+
+        if is_new_file:
+            self.tab_view.addTab(editor, "Untitled")
+            self.setWindowTitle("Untitled")
+            self.statusBar().showMessage("New file created", 2000)
+            self.tab_view.setCurrentIndex(self.tab_view.count() - 1)
+            self.current_file = None
+            return
+
+        if not path.is_file():
+            return
+        if self.is_binary(path):
+            self.statusBar().showMessage("Cannot open binary files", 2000)
+            return
+
+        # Check if the file is already open
+        for i in range(self.tab_view.count()):
+            if self.tab_view.tabText(i) == path.name:
+                self.tab_view.setCurrentIndex(i)
+                self.current_file = path
+                return
+
+        # Create new tab
+        self.tab_view.addTab(editor, path.name)
+        if not is_new_file:
+            with open(path, "r", encoding="utf-8") as file:
+                editor.setText(file.read())
+        self.setWindowTitle(path.name)
+        self.current_file = path
+        self.tab_view.setCurrentIndex(self.tab_view.count() - 1)
+        self.statusBar().showMessage(f"Opened {path}", 2000)
 
     def set_up_menu(self):
         menu_bar = self.menuBar()  # Get the menu bar of the window
@@ -62,6 +129,17 @@ class MainWindow(QMainWindow):
         open_file.setShortcut("Ctrl+O")
         open_file.triggered.connect(self.open_file)
 
+        # Save File
+        file_menu.addSeparator()
+        save_file = file_menu.addAction("Save")
+        save_file.setShortcut("Ctrl+S")
+        save_file.triggered.connect(self.save_file)
+
+        # Save As
+        save_as = file_menu.addAction("Save As")
+        save_as.setShortcut("Ctrl+Shift+S")
+        save_as.triggered.connect(self.save_as)
+
         # Open Folder
         open_folder = file_menu.addAction("Open Folder")
         open_folder.setShortcut("Ctrl+K")
@@ -75,16 +153,61 @@ class MainWindow(QMainWindow):
         copy_action.triggered.connect(self.copy)
 
     def new_file(self):
-        pass
+        self.set_new_tab(None, is_new_file=True)
 
     def open_file(self):
-        pass
+        ops = QFileDialog.Options()  # Create a file dialog
+        ops |= QFileDialog.DontUseNativeDialog
+        new_file, _ = QFileDialog.getOpenFileName(
+            self, "Pick a file", "", "All Files (*)", options=ops
+        )
+
+        if new_file == "":
+            self.statusBar().showMessage("Cancelled", 2000)
+            return
+        f = Path(new_file)
+        self.set_new_tab(f)
+
+    def save_file(self):
+        if self.current_file is None and self.tab_view.count() > 0:
+            self.save_as()
+
+        editor = self.tab_view.currentWidget()
+        self.current_file.write_text(editor.text())
+        self.statusBar().showMessage(f"Saved {self.current_file}", 2000)
+
+    def save_as(self):
+        editor = self.tab_view.currentWidget()
+        if editor is None:
+            return
+
+        file_path = QFileDialog.getSaveFileName(self, "Save as", os.getcwd())[0]
+        if file_path == "":
+            self.statusBar().showMessage("Cancelled", 2000)
+            return
+        path = Path(file_path)
+        path.write_text(editor.text())
+        self.tab_view.setTabText(self.tab_view.currentIndex(), path.name)
+        self.statusBar().showMessage(f"Saved {path}", 2000)
+        self.current_file = path
 
     def open_folder(self):
-        pass
+        ops = QFileDialog.Options()  # Create a file dialog
+        ops |= QFileDialog.DontUseNativeDialog
+
+        new_folder, _ = QFileDialog.getExistingDirectory(
+            self, "Pick a folder", "", options=ops
+        )
+        if new_folder:
+            self.model.setRootPath(new_folder)
+            self.tree_view.setRootIndex(self.model.index(new_folder))
+            self.statusBar().showMessage(f"Opened {new_folder}", 2000)
 
     def copy(self):
-        pass
+        editor = self.tab_view.currentWidget()
+        if editor is None:
+            return
+        editor.copy()
 
     def set_up_body(self):
         # Body
@@ -114,6 +237,16 @@ class MainWindow(QMainWindow):
         side_bar_layout.setContentsMargins(5, 10, 5, 0)
         side_bar_layout.setSpacing(0)
         side_bar_layout.setAlignment(Qt.AlignTop | Qt.AlignCenter)
+
+        # setup labels
+        folder_label = QLabel()
+        folder_label.setPixmap(
+            QPixmap("./src/icons/folder-icon-blue.svg").scaled(QSize(25, 25))
+        )
+        folder_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+        folder_label.setFont(self.window_font)
+        folder_label.mousePressEvent = self.show_hide_tab
+        side_bar_layout.addWidget(folder_label)
         self.side_bar.setLayout(side_bar_layout)
 
         body.addWidget(self.side_bar)
@@ -181,17 +314,33 @@ class MainWindow(QMainWindow):
         tree_frame_layout.addWidget(self.tree_view)
         self.tree_frame.setLayout(tree_frame_layout)
 
-        # Add tree view to split view
+        # Tab widget to add editor to
+        self.tab_view = QTabWidget()
+        self.tab_view.setContentsMargins(0, 0, 0, 0)
+        self.tab_view.setTabsClosable(True)
+        self.tab_view.setMovable(True)
+        self.tab_view.setDocumentMode(True)
+        self.tab_view.tabCloseRequested.connect(self.close_tab)
+
+        # Add tree view and tab view to split view
         self.hsplit.addWidget(self.tree_frame)
+        self.hsplit.addWidget(self.tab_view)
 
         body.addWidget(self.hsplit)
         body_frame.setLayout(body)
 
         self.setCentralWidget(body_frame)
 
+    def close_tab(self, index):
+        self.tab_view.removeTab(index)
+
+    def show_hide_tab(self, event): ...
+
     def tree_view_context_menu(self, position): ...
 
-    def tree_view_clicked(self): ...
+    def tree_view_clicked(self, index: QModelIndex):
+        path = self.model.filePath(index)
+        self.set_new_tab(Path(path))
 
 
 if __name__ == "__main__":
