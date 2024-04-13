4904d3d7ce67abb8f156f3a708ca7a855093c713
Emmanuel Munoz <emunozce80@gmail.com>
2024-04-12 18:10:54 -0600

    Refactor lexer.py to improve token recognition and error handling (num adn id)

--- lexer.py
+++ lexer.py
@@ -4,19 +4,34 @@ import re
 
 
 def get_tokens(file: Path):
+    """
+    Extracts tokens from a file and returns a list of tokens along with their positions.
 
+    Args:
+        file (Path): The path to the file to be processed.
+
+    Returns:
+        list: A list containing in [0] a list of dictionaries representing tokens
+        containing the token type and in [1] a list of dictionaries
+        representing errors and its position in the file.
+
+    Raises:
+        None
+
+    """
     with open(file, "r", encoding="utf-8") as f:
-        tokens = []  # Store the token
-        errors = []  # Store the error
+        tokens = []  # Store the tokens found
+        errors = []  # Store the errors
         position = []  # Store the position of the token in the file
 
         identifier_pattern = re.compile(r"^[a-zA-Z_][a-zA-Z0-9_]*$")
         reserved_words_pattern = re.compile(
             r"\b(if|else|do|while|switch|case|double|main|cin|cout|int|real|then|end|until)\b"
         )
-        number_pattern = re.compile(r"^\d+(\.\d+)?$")
+        number_pattern = re.compile(r"\b\d+\b")
         symbol_pattern = re.compile(r"\(|\)|,|{|}|;")
         assignment_pattern = re.compile(r"=")
+        logical_op_pattern = re.compile(r"\b(?:and|or)\b")
 
         lines = f.readlines()
 
@@ -26,7 +41,7 @@ def get_tokens(file: Path):
             ln = index + 1
 
             # Iterate over characters in the line
-            for char in string:
+            for index_string, char in enumerate(string):
                 if skip_col == 0:
                     if char == " ":
                         col += 1
@@ -43,7 +58,62 @@ def get_tokens(file: Path):
                         col += 1
                         continue
 
-                    if re.match(identifier_pattern, char):
+                    if re.match(assignment_pattern, char):
+                        if string[index_string + 1] == "=":
+                            tokens.append({"Logical Operator": "=="})
+                            position.append({"Line": ln, "Column": col})
+                            col += 2
+                            skip_col += 1
+                            continue
+
+                        tokens.append({"Assignment": char})
+                        position.append({"Line": ln, "Column": col})
+                        col += 1
+                        continue
+
+                    if re.match(number_pattern, char):
+                        number = ""
+                        number += char  # Add the first character to the number
+                        new_string = string[col:]  # Get the rest of the string
+
+                        i = 0
+                        while i < len(new_string) and re.match(
+                            number_pattern, new_string[i]
+                        ):
+                            number += new_string[i]
+                            col += 1
+                            skip_col += 1
+                            i += 1
+
+                        if (
+                            i < len(new_string)
+                            and new_string[i] == "."
+                            and re.match(number_pattern, new_string[i + 1])
+                        ):
+                            number += new_string[i]
+                            col += 2
+                            skip_col += 1
+                            i += 1
+                            while i < len(new_string) and re.match(
+                                number_pattern, new_string[i]
+                            ):
+                                number += new_string[i]
+                                col += 1
+                                skip_col += 1
+                                i += 1
+                            tokens.append({"Float Number": number})
+                            continue
+                        elif i < len(new_string) and new_string[i] != ".":
+                            tokens.append({"Integer Number": number})
+                            col += 1
+                            continue
+                        errors.append({"Error": char, "Line": ln, "Column": col})
+                        col += 1
+                        continue
+
+                    if re.match(
+                        identifier_pattern, char
+                    ):  # Check if it is an identifier, a reserved word or a logical operator
                         identifier = ""
                         identifier += char  # Add the first character to the identifier
                         new_string = string[col:]  # Get the rest of the string
@@ -56,6 +126,14 @@ def get_tokens(file: Path):
                             col += 1
                             skip_col += 1
                             i += 1
+
+                        if re.match(logical_op_pattern, identifier):
+                            tokens.append({"Logical Operator": identifier})
+                            position.append(
+                                {"Line": ln, "Column": col - len(identifier) + 1}
+                            )
+                            col += 1
+                            continue
 
                         if re.match(reserved_words_pattern, identifier):
                             tokens.append({"Reserved Word": identifier})
@@ -77,11 +155,7 @@ def get_tokens(file: Path):
                 else:
                     skip_col -= 1
 
-        for i, token in enumerate(tokens):
-            print(f"{token} {position[i]}\n")
-
-        # for i, error in enumerate(errors):
-        #     print(f"{error}\n")
+        return [tokens, errors]
 
 
 if __name__ == "__main__":
@@ -95,4 +169,15 @@ if __name__ == "__main__":
         if not file_path.exists():
             print("File does not exist")
         else:
-            get_tokens(file_path)
+            results = get_tokens(file_path)
+            int_numbers = [num for num in results[0] if "Integer Number" in num]
+            float_numbers = [num for num in results[0] if "Float Number" in num]
+
+            # for token in numbers:
+            #     print(f"{token}")
+
+            for token in results[0]:
+                print(f"{token}")
+
+            for token in results[1]:
+                print(f"{token}")
